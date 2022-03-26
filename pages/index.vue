6f7cf03b0071ec40ecaf55a6d6f5029852cbf3b8
mjeziorkowski <michaljeziorkowski@gmail.com>
2022-03-26 16:44:57 +0100

    Initial setup

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Nuxt with composition API & Tailwind</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({})
+</script>
